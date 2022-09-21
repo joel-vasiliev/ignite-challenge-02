9978b2040e4492aad7bb9c9bbaabb213bfc072e7
Joel <joel.neto@inmetrics.com.br>
2022-09-21 13:01:00 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+import { Button } from './components/Button';
+import { MovieCard } from './components/MovieCard';
+
+
+import './styles/global.scss';
+
+import './styles/sidebar.scss';
+import './styles/content.scss';
+import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+import { api } from './services/api';
+
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  useEffect(() => {
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      
+      <SideBar
+        selectedGenreId={selectedGenreId}
+        handleClickButton={handleClickButton}
+      />
+
+      <Content
+        selectedGenre={selectedGenre}
+        selectedGenreId={selectedGenreId}
+      />
+    </div>
+  )
+}
